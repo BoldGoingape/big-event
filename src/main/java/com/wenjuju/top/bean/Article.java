8720b6c7a330550d897a98d4f2358f339a1a4d08
bold-monkey <304925106@aliyun.com>
2024-02-22 13:36:13 +0800

    这是一次提交

--- src/main/java/com/wenjuju/top/bean/Article.java
+++ src/main/java/com/wenjuju/top/bean/Article.java
@@ -1,8 +1,11 @@
 package com.wenjuju.top.bean;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 import java.time.LocalDateTime;
 @Data
@@ -10,10 +13,17 @@ import java.time.LocalDateTime;
 //文章实现类
 public class Article {
     private Integer id;//主键id
+    @NotEmpty
+    @Pattern(regexp = "^\\S{1,10}$")
     private String title;//文章标题
+    @NotEmpty
     private String content;//文章内容
+    @NotEmpty
+    @URL
     private String coverImg;//文章封面
+
     private String state;//文章状态
+    @NotEmpty
     private Integer categoryId;//文章分类id
     private Integer createUser;//创建人id
     private LocalDateTime createTime;//创建时间
--- src/main/java/com/wenjuju/top/controller/ArticleController.java
+++ src/main/java/com/wenjuju/top/controller/ArticleController.java
@@ -7,6 +7,7 @@ import com.wenjuju.top.utils.jwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -31,7 +32,7 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
     @PostMapping
-    public Result add(@RequestBody Article article){
+    public Result add(@RequestBody @Validated Article article){
         articleService.add(article);
         return Result.success();
     }
